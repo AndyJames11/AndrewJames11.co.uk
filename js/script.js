7f9746f020e4b100804bcf633d1e5fbd9b47860d
AndyJames11 <andrewjamesh11@googlemail.com>
2024-06-07 15:26:14 +0100

    add functional navbar to testing page. re-link javascript files and images to correct pages.

--- js/script.js
+++ js/script.js
@@ -330,48 +330,24 @@ function reactionGame() {
 // INDEX PAGE DROPDOWN MENU
 
 document.addEventListener("DOMContentLoaded", function() {
-    const dropdownArrow = document.getElementById("dropdownArrow");
-    const dropdownList = document.getElementById("dropdownList");
+    const dropdownArrows = document.getElementsByClassName("navbarArrow");
 
-    function dropdown() {
-        if (dropdownList.style.display === "none" || dropdownList.style.display === "") {
-            dropdownList.style.display = "inline-block";
-            dropdownList.style.maxHeight = dropdownList.scrollHeight + "px";
-            dropdownArrow.classList.add("flipped");
-            dropdownList.style.transition = "max-height 1s ease-in-out";
-        } else {
-            dropdownList.style.maxHeight = "0px";
-            dropdownList.style.transition = "max-height 1s ease-in-out";
-            dropdownArrow.classList.remove("flipped");
-            setTimeout(() => {
-                dropdownList.style.display = "none";
-            }, 1000);
-        }
-    }
-
-    dropdownArrow.addEventListener("click", dropdown);
-});
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    const dropdownArrow = document.getElementById("navbarArrow");
-    const dropdownList = document.getElementById("dropdownList");
-
-    function dropdown() {
-        if (dropdownList.style.display === "none" || dropdownList.style.display === "") {
-            dropdownList.style.display = "inline-block";
-            dropdownList.style.maxHeight = dropdownList.scrollHeight + "px";
-            dropdownArrow.classList.add("flipped");
-            dropdownList.style.transition = "max-height 1s ease-in-out";
-        } else {
-            dropdownList.style.maxHeight = "0px";
-            dropdownList.style.transition = "max-height 1s ease-in-out";
-            dropdownArrow.classList.remove("flipped");
-            setTimeout(() => {
-                dropdownList.style.display = "none";
-            }, 1000);
-        }
-    }
-
-    dropdownArrow.addEventListener("click", dropdown);
+    Array.from(dropdownArrows).forEach((arrow, index) => {
+        arrow.addEventListener("click", function() {
+            const dropdownList = arrow.nextElementSibling;
+            if (dropdownList.style.display === "none" || dropdownList.style.display === "") {
+                dropdownList.style.display = "inline-block";
+                dropdownList.style.maxHeight = dropdownList.scrollHeight + "px";
+                arrow.classList.add("flipped");
+                dropdownList.style.transition = "max-height 1s ease-in-out";
+            } else {
+                dropdownList.style.maxHeight = "0px";
+                dropdownList.style.transition = "max-height 1s ease-in-out";
+                arrow.classList.remove("flipped");
+                setTimeout(() => {
+                    dropdownList.style.display = "none";
+                }, 1000);
+            }
+        });
+    });
 });
