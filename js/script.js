d1d24a1e793619ebcffcd5fd158d2fc973b0ad98
AndyJames11 <andrewjamesh11@googlemail.com>
2024-06-07 10:57:02 +0100

    begin building responsive navbar for navigation between each page

--- js/script.js
+++ js/script.js
@@ -221,8 +221,6 @@ function getVisiblePositions() {
 
 // Numbers allowed because of shirt number - Readme
 
-// Navbar with each javascript thing on a different page 
-
 // Collapsible sections for descriptions on each JavaScript section
 
 // Possible dark/light mode switch
@@ -333,6 +331,30 @@ function reactionGame() {
 
 document.addEventListener("DOMContentLoaded", function() {
     const dropdownArrow = document.getElementById("dropdownArrow");
+    const dropdownList = document.getElementById("dropdownList");
+
+    function dropdown() {
+        if (dropdownList.style.display === "none" || dropdownList.style.display === "") {
+            dropdownList.style.display = "inline-block";
+            dropdownList.style.maxHeight = dropdownList.scrollHeight + "px";
+            dropdownArrow.classList.add("flipped");
+            dropdownList.style.transition = "max-height 1s ease-in-out";
+        } else {
+            dropdownList.style.maxHeight = "0px";
+            dropdownList.style.transition = "max-height 1s ease-in-out";
+            dropdownArrow.classList.remove("flipped");
+            setTimeout(() => {
+                dropdownList.style.display = "none";
+            }, 1000);
+        }
+    }
+
+    dropdownArrow.addEventListener("click", dropdown);
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const dropdownArrow = document.getElementById("navbarArrow");
     const dropdownList = document.getElementById("dropdownList");
 
     function dropdown() {
